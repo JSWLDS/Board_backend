410812293fd08bd03d2825293365dac9021a8702
JSW <jswlds2@gmail.com>
2023-11-17 17:00:31 +0900

    2023-11-17

--- src/main/java/com/example/rest_react_practice/Entity/BoardPosts.java
+++ src/main/java/com/example/rest_react_practice/Entity/BoardPosts.java
@@ -24,7 +24,7 @@ public class BoardPosts {
     @Column(name = "board_id")
     private Long boardId;
 
-    @Column(name = "type_number")
+    @Column(name = "type_nu mber")
     private Long typeNo;
 
     @Column(name = "title")
